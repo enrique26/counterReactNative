34189d5d86483bf66d6261c7efa2e6c455fa791a
enrique26 <enriqueibilis@gmail.com>
2019-08-14 17:43:53 -0500

    agregando redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,35 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
-import loggerMiddleware from '../middleware/logger'
+// import loggerMiddleware from '../middleware/logger'
 //no es necesario crear un logger con redu ya exite uno
-import loggerMiddleware2 from 'redux-logger';
+import loggerMiddleware from 'redux-logger';
+import AsyncStorage from '@react-native-community/async-storage';
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
 
-const store = createStore(rootReducer,applyMiddleware(loggerMiddleware2))
 
-export default store;
+// const store = createStore(rootReducer,applyMiddleware(loggerMiddleware2))
+//
+// export default store;
+
+
+///configuracion inicial para el persistStore
+const persistConfig = {
+  key: 'root',
+  storage:AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const InitialState={}
+
+
+let store = createStore(persistedReducer,InitialState,applyMiddleware(loggerMiddleware))
+const persistor = persistStore(store
+  // , () => {
+  // // if you want to get restoredState
+  // console.log("restoredState", store.getState());
+// }
+);
+
+export { store, persistor }
